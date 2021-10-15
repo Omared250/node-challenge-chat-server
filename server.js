c9e8f519ee3076146bf6201795b40967c5308313
Omar <omared250@gmail.com>
2021-10-15 22:13:17 +0200

    post, put, delete

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ const welcomeMessage = {
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
 
+const isValidMessage = (newMessage) => {
+  if(newMessage.text && newMessage.from) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
@@ -34,6 +42,49 @@ app.get("/messages/:id", function (request, response) {
     response.status(404).send("Sorry! we cannot find this given id!!");
   }
 } )
+
+app.post("/messages", function (request, response) {
+  const messageNewId = Math.max(...messages.map((m) => m.id));
+  
+  const newMessage = {
+    id : messageNewId + 1,
+    from : request.body.from,
+    text : request.body.text,
+  }
+
+  if (!isValidMessage(newMessage)) {
+    response.status(404).send("Sorry!!, the message is not completed!!");
+    return;
+  }
+
+  messages.push(newMessage);
+  response.status(201).send("The new message have been created!!")
+})
+
+app.delete("/messages/:id", function (request, response) {
+
+  const messageId = parseInt(request.params.id);
+
+  const messageFound = messages.find((message) => message.id === messageId);
+
+  if (messageFound) {
+    messages.splice(messages.indexOf(messageFound), 1);
+    response.status(201).send("The message have been deleted!!!")
+  } else {
+    response.status(404).send("Sorry!!, This given id does not exist!!")
+  }
+})
+
+app.put("/messages/:id", function (request, response) {
+  const messageId = parseInt(request.params.id);
+
+  const findingMessage = messages.find((message) => message.id === messageId);
+
+  findingMessage.from = request.body.from;
+  findingMessage.text = request.body.text;
+
+  response.status(201).send("Message have been update!!!");
+})
 
 app.listen(3000, () => {
    console.log("Listening on port 3000")
